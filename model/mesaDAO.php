3c6a9d7e4892a0484fc0831acc9834282c58fc53
davidjuan99 <david.juanaranda@gmail.com>
2020-11-03 16:59:27 +0100

    Hecho filtro de ver solo disponibles por sala en función y añadido select en zona admin

--- model/mesaDAO.php
+++ model/mesaDAO.php
@@ -13,8 +13,8 @@ class MesaDao{
 		$sentencia=$pdo->prepare($sql);
 		$sentencia->execute();
 
-		$lista_alumno=$sentencia->fetchAll(PDO::FETCH_ASSOC);
-		foreach ($lista_alumno as $mesa) {
+		$lista_mesa=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+		foreach ($lista_mesa as $mesa) {
 			$id=$mesa["id_mesa"]." ";
 		/*	echo $id;*/
 		//$enviar=$enviar."'>Modificar</a>";
@@ -40,6 +40,39 @@ class MesaDao{
 			// echo "<td>{$mesa['plazas_mesa']}</td>";
 			// echo "<td>{$mesa['disponible_mesa']}</td>";
 			// echo "</tr>";
+		}
+
+   		}
+	}
+
+	public function filtroLugar(){
+		include '../model/connection.php';
+		if (!empty($_POST['disponible'])) {
+			$disponible = 1;
+			$sql="SELECT * FROM mesa where lugar_mesa LIKE '%{$_POST['lugares']}%' AND disponible_mesa LIKE '%{$disponible}%' ";
+		} else {
+			$sql="SELECT * FROM mesa where lugar_mesa LIKE '%{$_POST['lugares']}%' ";
+		}
+		$sentencia=$pdo->prepare($sql);
+		$sentencia->execute();
+
+		$lista_mesa=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+		foreach ($lista_mesa as $mesa) {
+			$id=$mesa["id_mesa"]." ";
+		/*	echo $id;*/
+		//$enviar=$enviar."'>Modificar</a>";
+		if ($mesa['disponible_mesa']==1) {
+			echo "<div style=float:left;display:block;position:relative;>";
+			echo "<img src='../img/mesa_verde.png' style='width:250px;height:250px;'>";
+			echo "<div style='position:absolute;top: 50%;left:50%;'>".$mesa['lugar_mesa']."<br>".$mesa['plazas_mesa']."</div>";
+			echo "</div>";
+			echo "</div>";
+		} else {
+			echo "<div style=float:left;display:block;position:relative;>";
+			echo "<img src='../img/mesa_roja.png' style='width:250px;height:250px;'>";
+			echo "<div style='position:absolute;top: 50%;left:50%;'>".$mesa['lugar_mesa']."<br>".$mesa['plazas_mesa']."</div>";
+			echo "</div>";
+			echo "</div>";
 		}
 
    		}
--- view/zona.admin.php
+++ view/zona.admin.php
@@ -7,10 +7,36 @@
 	<?php
 		require_once '../controller/session_controller.php';
 	?>
+	<form action="./zona.admin.php" method="POST">
+		<?php
+  		include '../model/connection.php';
+		$sql="SELECT distinct lugar_mesa FROM mesa";
+		$sentencia=$pdo->prepare($sql);
+		$sentencia->execute();
+
+		$lista_filtro=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+		echo "<select name='lugares' style='float:left';>";
+		foreach ($lista_filtro as $lugar) {
+			echo "<option value='".$lugar['lugar_mesa']."'>".$lugar['lugar_mesa']."</option>";
+		}
+			echo "<option value='Todos'>Todos</option>";
+		echo "</select><br><br>";
+		?>
+	<input type="checkbox" id="disponible" name="disponible" value="disponible">
+	<label for="disponible"> Disponible</label><br><br>
+	<input type="submit" value="Filtrar" name="b_filtro"><br><br>
 	<?php
 		include '../model/mesaDAO.php';
-		$mostrar_mesa=new MesaDao;
-		echo $mostrar_mesa->mostrar();
+		if (empty($_POST['b_filtro'])){
+			$mostrar_mesa=new MesaDao;
+			echo $mostrar_mesa->mostrar();
+		} else if ($_POST['lugares']=='Todos') {
+    		$mostrar_mesa=new MesaDao;
+			echo $mostrar_mesa->mostrar();
+		} else if ($_POST['lugares']!='Todos'){
+        	$filtros_alu=new MesaDao;
+        	$filtros_alu->filtroLugar();
+ 		}
 	?>
 </body>
 </html>
