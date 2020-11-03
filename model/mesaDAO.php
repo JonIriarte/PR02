4b399adc42062493f8f45fd94a93609ceef71ca7
davidjuan99 <david.juanaranda@gmail.com>
2020-11-03 15:51:31 +0100

    Visualización mesas verdes/rojas

--- model/mesaDAO.php
+++ model/mesaDAO.php
@@ -19,17 +19,27 @@ class MesaDao{
 		/*	echo $id;*/
 		//$enviar=$enviar."'>Modificar</a>";
 		if ($mesa['disponible_mesa']==1) {
-			echo "<tr style='color:green;text-align:center;'>";
-			echo "<td>{$mesa['lugar_mesa']}</td>";
-			echo "<td>{$mesa['plazas_mesa']}</td>";
-			echo "<td>{$mesa['disponible_mesa']}</td>";
-			echo "</tr>";
+			echo "<div style=float:left;display:block;position:relative;>";
+			echo "<img src='../img/mesa_verde.png' style='width:250px;height:250px;'>";
+			echo "<div style='position:absolute;top: 50%;left:50%;'>".$mesa['lugar_mesa']."<br>".$mesa['plazas_mesa']."</div>";
+			echo "</div>";
+			echo "</div>";
+			// echo "<tr style='color:green;text-align:center;'>";
+			// echo "<td>{$mesa['lugar_mesa']}</td>";
+			// echo "<td>{$mesa['plazas_mesa']}</td>";
+			// echo "<td>{$mesa['disponible_mesa']}</td>";
+			// echo "</tr>";
 		} else {
-			echo "<tr style='color:red;text-align:center;'>";
-			echo "<td>{$mesa['lugar_mesa']}</td>";
-			echo "<td>{$mesa['plazas_mesa']}</td>";
-			echo "<td>{$mesa['disponible_mesa']}</td>";
-			echo "</tr>";
+			echo "<div style=float:left;display:block;position:relative;>";
+			echo "<img src='../img/mesa_roja.png' style='width:250px;height:250px;'>";
+			echo "<div style='position:absolute;top: 50%;left:50%;'>".$mesa['lugar_mesa']."<br>".$mesa['plazas_mesa']."</div>";
+			echo "</div>";
+			echo "</div>";
+			// echo "<tr style='color:red;text-align:center;'>";
+			// echo "<td>{$mesa['lugar_mesa']}</td>";
+			// echo "<td>{$mesa['plazas_mesa']}</td>";
+			// echo "<td>{$mesa['disponible_mesa']}</td>";
+			// echo "</tr>";
 		}
 
    		}
--- view/zona.admin.php
+++ view/zona.admin.php
@@ -7,17 +7,10 @@
 	<?php
 		require_once '../controller/session_controller.php';
 	?>
-	<table width='40%'>
-		<tr>
-        	<th>Lugar mesa</th>
-        	<th>Cantidad sillas mesa</th>
-        	<th>Disponibilidad</th>
-     	</tr>
 	<?php
 		include '../model/mesaDAO.php';
 		$mostrar_mesa=new MesaDao;
 		echo $mostrar_mesa->mostrar();
 	?>
-	</table>
 </body>
 </html>
