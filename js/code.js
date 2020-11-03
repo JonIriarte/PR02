570c7868066af92aa642dc91ebc049cce14aa5f7
Pablo Soriano Antón <10006712.joan23@fje.edu>
2020-11-03 16:34:57 +0100

    Login y zon.admin

---
+++ js/code.js
@@ -0,0 +1,67 @@
+function validacionForm() {
+
+    var email=document.getElementById('nombre').value;
+
+    var password=document.getElementById('password').value;
+
+    var message=document.getElementById('message').value;
+
+    if (nombre=='' &&function validacionForm() {
+
+        var email=document.getElementById('nombre').value;
+    
+        var password=document.getElementById('password').value;
+    
+        var message=document.getElementById('message').value;
+    
+        if (nombre=='' && password=='') {
+            document.getElementById("message").innerHTML = "Inténtalo de nuevo.";
+            document.getElementsByTagName("label")[0].style.color = "red";
+            document.getElementsByTagName("label")[1].style.color = "red";
+            document.getElementById("email").style.borderColor = "red";
+            document.getElementById("passwd").style.borderColor = "red";
+    
+        } else if (nombre=='') {
+            document.getElementById("message").innerHTML = "Email del usuario";
+            document.getElementsByTagName("label")[0].style.color = "red";
+            document.getElementsByTagName("label")[1].style.color = "black";
+            document.getElementById("email").style.borderColor = "red";
+            document.getElementById("passwd").style.borderColor = "white";
+        } else if (password=='') {
+            document.getElementById("message").innerHTML = "El password está vacio";
+            document.getElementsByTagName("label")[0].style.color = "black";
+            document.getElementsByTagName("label")[1].style.color = "red";
+            document.getElementById("passwd").style.borderColor = "red";
+            document.getElementById("email").style.borderColor = "white";
+        } else {
+            return true;
+        }
+        document.getElementById("submit").style.color = "red";
+        document.getElementById("submit").style.backgroundColor = "#FFB0AE";
+        return false;
+    } password=='') {
+        document.getElementById("message").innerHTML = "Inténtalo de nuevo.";
+        document.getElementsByTagName("label")[0].style.color = "red";
+        document.getElementsByTagName("label")[1].style.color = "red";
+        document.getElementById("email").style.borderColor = "red";
+        document.getElementById("passwd").style.borderColor = "red";
+
+    } else if (nombre=='') {
+        document.getElementById("message").innerHTML = "Email del usuario";
+        document.getElementsByTagName("label")[0].style.color = "red";
+        document.getElementsByTagName("label")[1].style.color = "black";
+        document.getElementById("email").style.borderColor = "red";
+        document.getElementById("passwd").style.borderColor = "white";
+    } else if (password=='') {
+        document.getElementById("message").innerHTML = "El password está vacio";
+        document.getElementsByTagName("label")[0].style.color = "black";
+        document.getElementsByTagName("label")[1].style.color = "red";
+        document.getElementById("passwd").style.borderColor = "red";
+        document.getElementById("email").style.borderColor = "white";
+    } else {
+        return true;
+    }
+    document.getElementById("submit").style.color = "red";
+    document.getElementById("submit").style.backgroundColor = "#FFB0AE";
+    return false;
+}
