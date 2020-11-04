6d2734306666e422a12d34b2b5ec4ee50c7d53f7
Jon Iriarte <joniriartesulleiro@gmail.com>
2020-11-04 19:27:51 +0100

    Subida con el logout más en su sitio y correcciones de estilo visual

--- controller/session_controller.php
+++ controller/session_controller.php
@@ -4,5 +4,6 @@ session_start();
 if (!isset($_SESSION['nombre'])) {
     header('Location:../view/login.php');//sino está seteada la variable nombre_camarero volvemos al login.
 }
-echo '<div><h1>Bienvenido '.$_SESSION['nombre']->getNombre().'</h1><h1 style="float: right;"><a href="../controller/logout_controller.php">Logout</a></h1></div>';   
+echo '<div><h1 style="float:left;">Bienvenido, '.$_SESSION['nombre']->getNombre().'</h1><h1><a href="../controller/logout_controller.php">Logout</a></h1></div><br><br>';
+
 ?>
