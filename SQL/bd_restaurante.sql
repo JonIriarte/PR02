e731784f12d833a0ddd09ee154e6ee362bd0ec60
Jon Iriarte <joniriartesulleiro@gmail.com>
2020-11-04 18:41:42 +0100

    Subida con el index.php en la carpeta principal

---
+++ SQL/bd_restaurante.sql
@@ -0,0 +1,150 @@
+
+CREATE TABLE `camarero` (
+  `id_camarero` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO `camarero` (`nombre`, `password`) VALUES
+('david', '1fa3356b1eb65f144a367ff8560cb406'),
+('camarero1', '1fa3356b1eb65f144a367ff8560cb406'),
+('camarero2', '1fa3356b1eb65f144a367ff8560cb406'),
+('camarero3', '1fa3356b1eb65f144a367ff8560cb406');
+
+
+CREATE TABLE `mesa` (
+  `id_mesa` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `disponible_mesa` tinyint(1) NOT NULL,
+  `plazas_mesa` enum('1','2','4') NOT NULL,
+  `lugar_mesa` enum('Terraza','Barra','Sala','Sofá') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+CREATE TABLE `incidencia` (
+  `id_incidencia` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `descripcion_incidencia` text NOT NULL,
+  `estado_incidencia` enum('abierta','solucionada','en progreso','cerrada') NOT NULL,
+  `fecha_incidencia` datetime NOT NULL,
+  `id_mesa` int(11) NOT NULL COMMENT 'Depende de la tabla "Mesa"', 
+  Constraint fk_id_incidencia FOREIGN KEY (id_mesa)
+  references mesa(id_mesa)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
+
+INSERT INTO `mesa` (`disponible_mesa`, `plazas_mesa`, `lugar_mesa`) VALUES
+(1, '1', 'Barra'),
+(1, '1', 'Barra'),
+(1, '1', 'Barra'),
+(1, '1', 'Barra'),
+(1, '1', 'Barra'),
+(1, '1', 'Barra'),
+(1, '1', 'Barra'),
+(1, '1', 'Barra'),
+(1, '4', 'Sofá'),
+(1, '4', 'Sofá'),
+(1, '4', 'Sofá'),
+(1, '4', 'Sofá'),
+(1, '4', 'Sofá'),
+(1, '4', 'Sofá'),
+(1, '4', 'Sala'),
+(1, '4', 'Sala'),
+(1, '4', 'Sala'),
+(1, '4', 'Sala'),
+(1, '4', 'Sala'),
+(1, '4', 'Sala'),
+(1, '4', 'Sala'),
+(1, '4', 'Sala'),
+(1, '4', 'Sala'),
+(1, '2', 'Sala'),
+(1, '2', 'Sala'),
+(1, '2', 'Sala'),
+(1, '2', 'Sala'),
+(1, '2', 'Sala'),
+(1, '2', 'Sala'),
+(1, '4', 'Terraza'),
+(1, '4', 'Terraza'),
+(1, '4', 'Terraza'),
+(1, '4', 'Terraza'),
+( 1, '4', 'Terraza'),
+(1, '4', 'Terraza');
+
+
+CREATE TABLE `reserva` (
+  `id_reserva` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `fecha_reserva` datetime NOT NULL,
+  `fin_reserva` datetime DEFAULT NULL,
+ CONSTRAINT `ch_reserva` CHECK (`fecha_reserva`< `fin_reserva`),
+  `id_camarero` int(11) NOT NULL COMMENT 'Depende de la tabla "camarero"',
+  `id_mesa` int(11) NOT NULL COMMENT 'Depende de la tabla "mesa"', 
+  Constraint fk_id_camarero FOREIGN KEY (id_camarero)
+  references camarero(id_camarero), 
+  CONSTRAINT fk_id_reserva FOREIGN KEY (id_mesa)
+  REFERENCES mesa (id_mesa) 
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+--
+/*ALTER TABLE `incidencia` REFERENCES mesa (id_mesa) 
+  ADD KEY `fk_id_mesa` (`id_mesa`);
+
+--
+-- Indices de la tabla `mesa`
+
+--
+-- Indices de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD KEY `fk_id_mesa` (`id_mesa`) USING BTREE,
+  ADD KEY `fk_id_camarero` (`id_camarero`) USING BTREE;
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `camarero`
+--
+ALTER TABLE `camarero`
+  MODIFY `id_camarero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `incidencia`
+--
+ALTER TABLE `incidencia`
+  MODIFY `id_incidencia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mesa`
+--
+ALTER TABLE `mesa`
+  MODIFY `id_mesa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `incidencia`
+--
+ALTER TABLE `incidencia`
+  ADD CONSTRAINT `fk_id_mesa` FOREIGN KEY (`id_mesa`) REFERENCES `mesa` (`id_mesa`);
+
+--
+-- Filtros para la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD CONSTRAINT `fk_id_camarero` FOREIGN KEY (`id_camarero`) REFERENCES `camarero` (`id_camarero`),
+  ADD CONSTRAINT `fk_id_mes` FOREIGN KEY (`id_mesa`) REFERENCES `mesa` (`id_mesa`);
+*/
