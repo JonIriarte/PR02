88d8fd5af8c1b095c3d3bf9e318078abacb426a1
davidjuan99 <david.juanaranda@gmail.com>
2020-11-04 18:18:21 +0100

    Corregido query y añadido consulta con count de las mesas reservadas

--- model/reservaDAO.php
+++ model/reservaDAO.php
@@ -9,22 +9,36 @@ class ReservaDao{
     }
     public function mostrarReservas(){
     	include '../model/connection.php';
-    	$sql="SELECT * FROM reserva";
+    	$sql="SELECT reserva.id_reserva,reserva.fecha_reserva,reserva.fin_reserva,reserva.id_mesa,camarero.nombre from reserva INNER JOIN camarero on reserva.id_camarero=camarero.id_camarero";
 		$sentencia=$pdo->prepare($sql);
 		$sentencia->execute();
 
 		$lista_reserva=$sentencia->fetchAll(PDO::FETCH_ASSOC);
 		foreach ($lista_reserva as $reserva) {
 			$id_reserva=$reserva["id_mesa"]." ";
-			echo $reserva['id_reserva']." , ";
-			echo $reserva['fecha_reserva']." , ";
-			echo $reserva['fin_reserva']." , ";
-			echo $reserva['id_camarero']." , ";
-			echo $reserva['id_mesa']." ";
-			echo "<br>";
+			echo "<tr>";
+			echo "<td style='text-align:center'>".$reserva['id_reserva']."</td>";
+			echo "<td style='text-align:center'>".$reserva['fecha_reserva']."</td>";
+			echo "<td style='text-align:center'>".$reserva['fin_reserva']."</td>";
+			echo "<td style='text-align:center'>".$reserva['nombre']."</td>";
+			echo "<td style='text-align:center'>".$reserva['id_mesa']."</td>";
+			echo "</tr>";
 		}
 	}
 	public function contarReservas(){
-		
+		include '../model/connection.php';
+    	$sql="SELECT count(reserva.id_mesa) as 'Veces reservada',mesa.lugar_mesa, reserva.id_mesa from reserva inner join mesa on reserva.id_mesa=mesa.id_mesa group by id_mesa";
+		$sentencia=$pdo->prepare($sql);
+		$sentencia->execute();
+
+		$lista_contador=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+		foreach ($lista_contador as $contador) {
+			$id_reserva=$contador["id_mesa"]." ";
+			echo "<tr>";
+			echo "<td style='text-align:center'>".$contador['id_mesa']."</td>";
+			echo "<td style='text-align:center'>".$contador['lugar_mesa']."</td>";
+			echo "<td style='text-align:center'>".$contador['Veces reservada']."</td>";
+			echo "</tr>";
+		}
 	}
 }
--- view/mostrar_reserva.php
+++ view/mostrar_reserva.php
@@ -7,11 +7,30 @@
 	<?php 
 		require_once '../controller/session_controller.php'; 
 	?>
+	<table cellpadding="6" cellspacing="6">
+		<tr>
+			<th>Mesa</th>
+			<th>Lugar mesa</th>
+			<th>Veces reservada</th>
+		</tr>
 	<?php
 		include '../model/reservaDAO.php';
 		$mostrar_reserva=new ReservaDao;
+		echo $mostrar_reserva->contarReservas();
+	?>
+	<table cellpadding="6" cellspacing="6">
+		<tr>
+			<th>ID Reserva</th>
+			<th>Fecha Reserva</th>
+			<th>Fin Reserva</th>
+			<th>Nombre camarero</th>
+			<th>Mesa</th>
+		</tr>
+	<?php
+		$mostrar_reserva=new ReservaDao;
 		echo $mostrar_reserva->mostrarReservas();
 	?>
+	</table>
 	<br>
 	<button><a href="./zona.admin.php">Volver atrás</button></a>
 </body>
