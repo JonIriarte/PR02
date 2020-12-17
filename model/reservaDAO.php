da2e35298af61b2d649fe6868dd79193f3d168ce
Jon Iriarte <joniriartesulleiro@gmail.com>
2020-12-17 22:16:43 +0100

    Boton de anular reserva hecho. Ver Issue #25 para lo que falta

--- model/reservaDAO.php
+++ model/reservaDAO.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 require_once 'mesa.php';
 require_once 'reserva.php';
 class ReservaDao{
@@ -14,13 +14,13 @@ class ReservaDao{
 		$sentencia=$pdo->prepare($sql);
 		$sentencia->execute();
 		$lista_reserva=$sentencia->fetchAll(PDO::FETCH_ASSOC);
-			echo "<table>"; 
-			echo "<td style='text-align:center'>Id reserva</td>"; 
+			echo "<table>";
+			echo "<td style='text-align:center'>Id reserva</td>";
 			echo "<td style='text-align:center'>Teléfono de contacto</td>";
 			echo "<td style='text-align:center'>Nombre</td>";
 			echo "<td style='text-align:center'>Fecha</td>";
-			echo "<td style='text-align:center'>Hora</td>"; 
-			echo "<td style='text-align:center'>Mesa</td>"; 
+			echo "<td style='text-align:center'>Hora</td>";
+			echo "<td style='text-align:center'>Mesa</td>";
 		foreach ($lista_reserva as $reserva) {
 			$id_reserva=$reserva["id_reserva"]." ";
 			echo "<tr>";
@@ -30,7 +30,6 @@ class ReservaDao{
 			echo "<td style='text-align:center'>".$reserva['fecha_reserva']."</td>";
 			echo "<td style='text-align:center'>".$reserva['hora_reserva']."</td>";
 			echo "<td style='text-align:center'>".$reserva['id_mesa']."</td>";
-			//Hay que hacer un botón para eliminar reservas AQUÍ
 			echo "</tr>";
 		}
 	}
@@ -52,74 +51,106 @@ class ReservaDao{
 	public function hacerReserva($dia,$hora,$nombre,$telefono,$id_mesa){
 		$reserva=new Reserva($dia,$hora,$nombre,$telefono);
 		$reserva->setIdMesa($id_mesa);
-		$reserva->setFechaReserva($dia); 
-		//Comprobación de que la fecha y la hora de la reserva sea posterior al momento actual. 
-		$UNIXHoy=time(); 
+		$reserva->setFechaReserva($dia);
+		//Comprobación de que la fecha y la hora de la reserva sea posterior al momento actual.
+		$UNIXHoy=time();
 		//Convertir el día de la reserva a tiempo UNIX
-		$fechaReserva=strtotime($dia); 
+		$fechaReserva=strtotime($dia);
 		//Segundos del día que han pasado desde el inicio hasta cada franja que se puede reservar
-		$f1=46800; 
-		$f2=50400; 
-		$f3=54000; 
-		$f4=72000; 
-		$f5=75600; 
-		
+		$f1=46800;
+		$f2=50400;
+		$f3=54000;
+		$f4=72000;
+		$f5=75600;
+
 		if($hora='13'){
-			$UNIXHoy=$UNIXHoy+$f1; 
-			$fechaReserva=$fechaReserva+$f1; 
+			$UNIXHoy=$UNIXHoy+$f1;
+			$fechaReserva=$fechaReserva+$f1;
 		}elseif($hora='14'){
-			$UNIXHoy=$UNIXHoy+$f2; 
+			$UNIXHoy=$UNIXHoy+$f2;
 			$fechaReserva=$fechaReserva+$f2;
 		}elseif($hora='15'){
-			$UNIXHoy=$UNIXHoy+$f3; 
+			$UNIXHoy=$UNIXHoy+$f3;
 			$fechaReserva=$fechaReserva+$f3;
 		}elseif($hora='20'){
-			$UNIXHoy=$UNIXHoy+$f4; 
+			$UNIXHoy=$UNIXHoy+$f4;
 			$fechaReserva=$fechaReserva+$f4;
 		}else{
-			$UNIXHoy=$UNIXHoy+$f5; 
+			$UNIXHoy=$UNIXHoy+$f5;
 			$fechaReserva=$fechaReserva+$f5;
 		}
-		if($fechaReserva>$UNIXHoy){	
+		if($fechaReserva>$UNIXHoy){
 			try {
-			
-			 	$prequery="SELECT * FROM tbl_reserva WHERE fecha_reserva='$dia' AND hora_reserva='$hora' AND id_mesa='$id_mesa'"; 
-			 	$prequery=$this->pdo->prepare($prequery); 
-			 	$prequery->execute(); 
-			 	$count=$prequery->rowCount();  
+
+			 	$prequery="SELECT * FROM tbl_reserva WHERE fecha_reserva='$dia' AND hora_reserva='$hora' AND id_mesa='$id_mesa'";
+			 	$prequery=$this->pdo->prepare($prequery);
+			 	$prequery->execute();
+			 	$count=$prequery->rowCount();
 				if ($count==0){
-						$query="INSERT INTO `tbl_reserva` (`fecha_reserva`,`hora_reserva`,`nombre_reserva`,`telf_reserva`,`id_mesa`) VALUES (?,?,?,?,?); "; 
-						$sentencia=$this->pdo->prepare($query); 
-						$sentencia->bindParam(1,$dia); 
+						$query="INSERT INTO `tbl_reserva` (`fecha_reserva`,`hora_reserva`,`nombre_reserva`,`telf_reserva`,`id_mesa`) VALUES (?,?,?,?,?); ";
+						$sentencia=$this->pdo->prepare($query);
+						$sentencia->bindParam(1,$dia);
 						$sentencia->bindParam(2,$hora);
 						$sentencia->bindParam(3,$nombre);
 						$sentencia->bindParam(4,$telefono);
 						$sentencia->bindParam(5,$id_mesa);
-						$sentencia->execute(); 
+						$sentencia->execute();
 						echo "RESERVA HECHA <br>";
-						echo "<br>"; 
-						echo "<a href='../view\zona.camareros.php'>VOLVER</a>"; 
+						echo "<br>";
+						echo "<a href='../view\zona.camareros.php'>VOLVER</a>";
 					}else{
 						header('Location:../view\zona.camareros.php');
 					}
 			 } catch (\Throwable $th) {
-				echo $th; 
-				echo "<br>"; 
-			 	echo "Tienes que rellenar todos los campos"; 
+				echo $th;
+				echo "<br>";
+			 	echo "Tienes que rellenar todos los campos";
 			 	echo "<br>";
-			 	echo "<a href='../view\zona.camareros.php'>VOLVER</a>"; 
+			 	echo "<a href='../view\zona.camareros.php'>VOLVER</a>";
 			 }
 		}else{
-			header('location: ../view/zona.camareros.php'); 
+			header('location: ../view/zona.camareros.php');
 		}
 	}
-	public function anularReserva($telefono){
-		
+	public function MostrarAnularReserva($telefono){
+		$reserva=new Reserva("","","",$telefono);
 
-
+		$query="SELECT * FROM `tbl_reserva` WHERE `telf_reserva` LIKE '%$telefono%'";
+		$sentencia=$this->pdo->prepare($query);
+		$sentencia->execute();
+		$lista_reserva=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+		$numero=$sentencia->rowCount();
+			if($numero==0){
+				echo "No hay reservas con ese teléfono";
+				echo "<a href='../view\zona.camareros.php'>VOLVER</a>";
+			}else{
+				echo "<table>";
+				echo "<td style='text-align:center'>Id reserva</td>";
+				echo "<td style='text-align:center'>Teléfono de contacto</td>";
+				echo "<td style='text-align:center'>Nombre</td>";
+				echo "<td style='text-align:center'>Fecha</td>";
+				echo "<td style='text-align:center'>Hora</td>";
+				echo "<td style='text-align:center'>Mesa</td>";
+			foreach ($lista_reserva as $resultado) {
+				$id_reserva=$resultado["id_reserva"];
+				echo "<tr>";
+				echo "<td style='text-align:center'>".$resultado['id_reserva']."</td>";
+				echo "<td style='text-align:center'>".$resultado['telf_reserva']."</td>";
+				echo "<td style='text-align:center'>".$resultado['nombre_reserva']."</td>";
+				echo "<td style='text-align:center'>".$resultado['fecha_reserva']."</td>";
+				echo "<td style='text-align:center'>".$resultado['hora_reserva']."</td>";
+				echo "<td style='text-align:center'>".$resultado['id_mesa']."</td>";
+				echo "<td><a href='../view/anular_reserva.php?id_reserva="."$id_reserva"."'>Eliminar</a></td>";
+				echo "</tr>";
+			}
+		}
 	}
 
-
-
-
+	public function AnularReserva($id){
+        $query="DELETE  FROM tbl_reserva WHERE id_reserva= ?; ";
+        $sentencia=$this->pdo->prepare($query);
+        $sentencia->bindParam(1,$id);
+        $sentencia->execute();
+        header('Location:../view\zona.camarero.php');
+    }
 }
--- view/anular_reserva.php
+++ view/anular_reserva.php
@@ -1,13 +1,17 @@
 <?php
+    require_once '../controller/session_controller.php';
+    include '../model/reservaDAO.php';  
 if (isset($_POST['telefono'])){
-    echo $_POST['telefono']; 
-
-    $anular=new ReservaDao
-    $anular->anularReserva($_POST['telefono']); 
+    $mostrar=new ReservaDao; 
+    echo $mostrar->MostrarAnularReserva($_POST['telefono']); 
+    
+    
+    // $anular=new ReservaDao(); 
+    // echo $anular->AnularReserva($_GET['id_reserva']); 
 
 }else{
     header('Location:./zona.camareros.php');
 }
-
-
+echo $_GET['id_reserva']; 
 ?>
+
